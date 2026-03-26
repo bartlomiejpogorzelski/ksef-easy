a7e34ba5bc93029994f5a54ba40dabc2795fc2c5
Bartlomiej <bartlomiej.pogorzelski@onet.pl>
2026-03-26 19:50:29 +0100

    Remove unused CurrencyHelper - method moved to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,15 @@
 module ApplicationHelper
   include Helpers::Base
-  include CurrencyHelper
 
   def current_theme
     :light
+  end
+
+  def format_money(amount)
+    return "" if amount.nil?
+
+    amount = amount.to_f.round(2)
+    formatted = number_with_delimiter(amount, delimiter: " ", separator: ",")
+    "#{formatted} zł"
   end
 end
